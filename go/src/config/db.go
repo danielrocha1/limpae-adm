271ea1dd43c1f3b89c5a55d93859a33b5fcb6492
Daniel Rocha <daniel.rochats@gmail.com>
2026-04-25 10:24:03 -0300

    security: remover seed de admin com senha hardcoded

--- go/src/config/db.go
+++ go/src/config/db.go
@@ -8,7 +8,6 @@ import (
 	"limpae/go/src/models"
 
 	"github.com/joho/godotenv"
-	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -59,32 +58,7 @@ func ConnectDB() {
 		log.Fatal("Erro ao alinhar tamanho de service_type:", err)
 	}
 
-	SeedAdmin()
-
 	fmt.Println("Banco de dados conectado e migrado com sucesso!")
-}
-
-func SeedAdmin() {
-	var admin models.User
-	email := "admin@limpae.com"
-	if err := DB.Where("email = ?", email).First(&admin).Error; err != nil {
-		// Admin não existe, vamos criar
-		hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("admin123"), bcrypt.DefaultCost)
-		admin = models.User{
-			Name:         "Administrador",
-			Email:        email,
-			PasswordHash: string(hashedPassword),
-			Role:         "admin",
-			Phone:        11999999999,
-			Cpf:          "00000000000",
-			Photo:        "https://ui-avatars.com/api/?name=Admin&background=0D8ABC&color=fff",
-		}
-		if err := DB.Create(&admin).Error; err != nil {
-			fmt.Printf("Aviso: Não foi possível criar o usuário admin padrão: %v\n", err)
-		} else {
-			fmt.Println("Usuário admin padrão criado com sucesso (admin@limpae.com / admin123)")
-		}
-	}
 }
 
 func ensureServiceTypeColumnLength() error {
